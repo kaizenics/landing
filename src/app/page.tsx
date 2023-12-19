972cc5db8476aa26997811337a74df19af674916
Niko <nicosejohnsoriano@gmail.com>
2023-12-20 01:58:59 +0800

    Add swr package and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import Image from "next/image";
+import bgImg from "~/images/its-bg-7.png";
 
 export default function Home() {
   return (
-   <main>
-    <p>HCDC - ITS</p>
-   </main>
-  )
+    <>
+      <main>
+        <section>
+          <p>HCDC - ITS</p>
+          <div className="absolute left-0 right-0 top-0 -z-20">
+            <Image 
+              src={bgImg}
+              priority
+              alt="bg img"
+              placeholder="blur"
+              draggable="false"
+              className="pointer-events-none -mb-96 mt-44 h-full w-full sm:mt-20 md:mt-0"
+            />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
