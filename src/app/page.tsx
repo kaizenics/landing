35746ce9deee62cbd905ebdf93bd8c58395780ad
Niko <nicosejohnsoriano@gmail.com>
2024-01-08 22:48:18 +0800

    res: refractor ui components and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,21 @@ export default function Home() {
     <>
       <main>
         <section>
-          <Container className="container mx-auto flex flex-col px-6 xl:px-0">
-            <div className="my-20">
-              <p className="font-montserrat mb-2 text-[12px] mb:text-[15px] mx-2 md:text-[25px]">
+          <Container className="container mx-auto flex flex-col">
+            <div className="container flex flex-col my-20">
+              <p className="font-montserrat mb-2 sm:mb-2 md:mb-2 lg:mb-1 xl:mb-1 text-[8px] sm:text-[14px] md:text-[18px] lg:text-[25px] xl:text-[25px] mx-2">
                 <span className="font-semibold">
-                  Holy Cross of Davao College{" "}
-                </span>
-                / Unofficial Publication Website
+                  Holy Cross of Davao College 
+                </span> / Unofficial Publication Website
               </p>
-
-              <div className="bg-primary-200 w-[90%] lg:max-w-[100%] p-1 rounded-full">
-                <h1 className="font-montserrat font-bold text-[20px] lg:text-7xl uppercase mx-4 md:text-5xl">
-                  Information Technology
+            
+              
+                <h1 className="font-montserrat font-bold text-[18px] sm:text-[30px] md:text-[45px] lg:text-[60px] xl:text-[65px] uppercase">
+                  <span className="bg-primary-200 w-[100%] p-2 sm:px-4 md:px-5 lg:px-5 xl:px-5 rounded-full">Information Technology</span>
                 </h1>
-              </div>
+  
 
-
-              <h1 className="font-montserrat font-semibold text-5xl lg:text-9xl uppercase md:text-7xl">
+              <h1 className="font-montserrat mt-1 font-semibold text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl uppercase ">
                 Society
               </h1>
             </div>
--- src/components/ui/Separator.tsx
+++
@@ -1,3 +0,0 @@
-export const Separator = ({ className }: { className?: string }) => {
-    return <div className={`h-[2px] bg-secondary-100 w-full ${className}`}></div>;
-  };
