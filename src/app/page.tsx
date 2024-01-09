b84f6c84f3bafa8fd2c3778c9d73d2b6dc192584
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-01-09 11:06:21 +0800

    Add Facebook page component
    add facebook page embed as component
    add comments for embed notes
    add custom scrollbar css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Publications } from "~/components/Publications";
 import { Container } from "~/components/ui/Container";
 import { MdExpandMore } from "react-icons/md";
 import "./index.css"
+import { FacebookPage } from "~/components/FacebookPage";
 
 export default function Home() {
   return (
@@ -15,15 +16,15 @@ export default function Home() {
             <div className="container flex flex-col my-20">
               <p className="font-montserrat mb-2 sm:mb-2 md:mb-2 lg:mb-1 xl:mb-1 text-[9px] sm:text-[14px] md:text-[18px] lg:text-[25px] xl:text-[25px] mx-2">
                 <span className="font-semibold">
-                  Holy Cross of Davao College 
+                  Holy Cross of Davao College
                 </span> / Official Publication Website
               </p>
-            
-              
-                <h1 className="font-montserrat font-bold text-[18px] sm:text-[30px] md:text-[45px] lg:text-[60px] xl:text-[65px] uppercase">
-                  <span className=" bg-gradient-to-r from-[#330000] via-[#e42121] to-[#4b0101] background-animate w-[100%] p-2 sm:px-4 md:px-5 lg:px-5 xl:px-5 rounded-full">Information Technology</span>
-                </h1>
-  
+
+
+              <h1 className="font-montserrat font-bold text-[18px] sm:text-[30px] md:text-[45px] lg:text-[60px] xl:text-[65px] uppercase">
+                <span className=" bg-gradient-to-r from-[#330000] via-[#e42121] to-[#4b0101] background-animate w-[100%] p-2 sm:px-4 md:px-5 lg:px-5 xl:px-5 rounded-full">Information Technology</span>
+              </h1>
+
               <h1 className="font-montserrat mt-1 font-semibold text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl uppercase ">
                 Society
               </h1>
@@ -40,8 +41,9 @@ export default function Home() {
 
         <About id="about" />
 
-
+        <FacebookPage id="facebookpage" />
         <Publications id="publications" />
+
       </main>
     </>
   );
