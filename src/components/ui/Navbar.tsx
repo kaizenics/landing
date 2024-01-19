65467529aa24012d231045bb5413273971270fc1
Niko <nicosejohnsoriano@gmail.com>
2024-01-20 02:40:06 +0800

    style: navbar logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -6,7 +6,7 @@ import { Container } from "~/components/ui/Container";
 import { Link } from "react-scroll/modules";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import Image from "next/image";
-import itslogo from "~/assets/images/New-ITS-Footer.png";
+import itslogo from "~/assets/images/ITS-Header.png";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,11 +18,7 @@ export const Navbar = () => {
   return (
     <Container className="md:flex flex-row justify-between container md:items-center py-12">
       <div className="flex justify-between items-center">
-        <Image
-          className="h-10 w-fit sm:h-20 sm:w-fit"
-          src={itslogo}
-          alt="HCDC-ITS Logo"
-        />
+      <h4 className="font-montserrat my-5 sm:my-5 md:my-5 lg:my-0 xl:my-0 font-semibold text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl mb-4 lg:mb-0">HCDC-ITS</h4>
         <div className="md:hidden">
           <button
             className="text-gray-500 hover:text-gray-700 focus:outline-none sm:py-[4px]"
