3d43f6558d2f7a9f3d98ee6d1ae81d37a8f2a6bd
kaizenics <nicosejohnsoriano@gmail.com>
2024-08-11 07:17:39 +0800

    chore: Remove unused Organizations component and update About component
    The Organizations component was deleted as it was no longer being used. Additionally, the About component was updated to remove the import of the Separator component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import { Publications } from "~/components/publications";
 import { Hero } from "~/components/hero";
 import { Partners } from "~/components/partners";
 import { Contributors } from '../components/contributors';
-import { Organizations } from "~/components/organizations";
-
 import "./index.css"
 
 
