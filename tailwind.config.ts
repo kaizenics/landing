9de090e6019418f326bb4637832d92f30da60197
kaizenics <nicosejohnsoriano@gmail.com>
2024-08-11 06:46:17 +0800

    chore: Update Tailwind CSS configuration
    Update the Tailwind CSS configuration file to include new color variables and styles for the container, border, input, ring, background, foreground, primary, secondary, destructive, muted, accent, popover, and card elements. Also, add new keyframes and animations for accordion, moveUp, appear, and marquee. Additionally, update the plugin dependencies to include tailwindcss-animate and addVariablesForColors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,103 @@
-// tailwind.config.ts
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss"
+//@ts-ignore
+import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette'
+import svgToDataUri from 'mini-svg-data-uri'
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme('colors'))
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]))
+
+  addBase({
+    ':root': newVars
+  })
+}
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        moveUp: {
+          '0%': { transform: 'translateY(5%)', opacity: '0' },
+          '100%': { transform: 'translateY(0%)', opacity: '1' }
+        },
+        appear: {
+          from: { opacity: '0' },
+          to: { opacity: '1' }
+        },
+        marquee: {
+          to: { transform: 'translateX(-50%)' }
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        moveUp: 'moveUp 1.4s ease forwards',
+        appear: 'appear 1s 1s forwards',
+        marquee: 'marquee var(--duration, 60s) linear infinite'
+      },
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif'],
         'montserrat-thin': ['Montserrat', 'sans-serif', '100'],
@@ -16,28 +105,24 @@ const config: Config = {
         'montserrat-bold': ['Montserrat', 'sans-serif', '700'],
         'montserrat-semibold': ['Montserrat', 'sans-serif', '600'],
       },
-      colors: {
-        primary: {
-          100: '#FFFCE1',
-          200: '#960202',
-        },
-        secondary: {
-          100: '#222222',
-          200: '#171717',
-        },
-      },
-      fontSize: {},
-      spacing: {},
-      width: {
-        '13/13': '75%',
-        '12/13': '96.3076923%',
-      },
     },
   },
-  darkMode: 'class', 
-  plugins: [],
-};
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme, addUtilities }: any) {
+      matchUtilities(
+        {
+          'bg-grid': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`
+          })
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      )
+    }
+  ]
+} satisfies Config
 
-export default config;
-
-
+export default config
